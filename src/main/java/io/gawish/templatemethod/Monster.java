d7d7c1e8f541f88a37fe324d3dc7ee7523f797c3
kagawish <karimgawish.1@gmail.com>
2022-01-10 11:40:38 +0200

    [Add] Small change

--- src/main/java/io/gawish/templatemethod/Monster.java
+++ src/main/java/io/gawish/templatemethod/Monster.java
@@ -2,15 +2,7 @@ package io.gawish.templatemethod;
 
 public class Monster implements Entity {
     public void takeTurn() {
-        this.collectApples();
-        this.attackNearestPlayer();
-    }
-
-    private void collectApples() {
         System.out.println("Monster is collecting apples");
-    }
-
-    private void attackNearestPlayer() {
         System.out.println("Monster attempts to attack nearest player");
     }
 }
