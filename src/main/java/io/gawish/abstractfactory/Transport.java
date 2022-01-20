b5af4cdfda4ed38d8b98ea76fe0fc69667151d8c
kagawish <karimgawish.1@gmail.com>
2022-01-20 06:50:32 +0200

    [Remove] Removes unused runners

--- src/main/java/io/gawish/abstractfactory/Transport.java
+++
@@ -1,6 +0,0 @@
-package io.gawish.abstractfactory;
-
-public interface Transport {
-    public abstract void visit();
-    public abstract void travel();
-}
--- src/main/java/io/gawish/builder/ElectricityGenerator.java
+++
@@ -1,9 +0,0 @@
-package io.gawish.builder;
-
-public class ElectricityGenerator {
-    private float generationRate;
-
-    public ElectricityGenerator(float generationRate) {
-        this.generationRate = generationRate;
-    }
-}
